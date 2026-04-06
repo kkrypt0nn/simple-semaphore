ab01b48fffff4b418cd6b4830c8c9cbba524dab5
Krypton <root@krypton.ninja>
2026-04-06 19:23:58 +0200

    fix: sync caveats, use mutex for permits

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 use std::{
-    sync::{
-        Arc, Condvar, Mutex,
-        atomic::{AtomicUsize, Ordering},
-    },
+    sync::{Arc, Condvar, Mutex},
     thread::available_parallelism,
 };
 
@@ -12,18 +9,16 @@ use std::{
 /// * When `try_acquire()` is called and the semaphore still has permits to give, it will return `Some(Permit)`. If there are no permits that can be given, it will return `None`.
 #[derive(Debug)]
 pub struct Semaphore {
-    permits: Arc<AtomicUsize>,
+    permits: Mutex<usize>,
     condvar: Condvar,
-    mutex: Mutex<()>,
 }
 
 impl Semaphore {
     /// Returns a new `Arc<Semaphore>` with the limit of permits chosen by you.
     pub fn new(permits: usize) -> Arc<Self> {
         Arc::new(Semaphore {
-            permits: Arc::new(AtomicUsize::new(permits)),
+            permits: Mutex::new(permits),
             condvar: Condvar::new(),
-            mutex: Mutex::new(()),
         })
     }
 
@@ -31,48 +26,51 @@ impl Semaphore {
     pub fn new_available_parallelism() -> Result<Arc<Self>, String> {
         match available_parallelism() {
             Ok(parallelism) => Ok(Arc::new(Semaphore {
-                permits: Arc::new(AtomicUsize::new(parallelism.get())),
+                permits: Mutex::new(parallelism.get()),
                 condvar: Condvar::new(),
-                mutex: Mutex::new(()),
             })),
             Err(err) => Err(err.to_string()),
         }
     }
 
     /// Returns the number of available permits
+    #[deprecated(
+        since = "1.1.0",
+        note = "Please do not use this method anymore, there will be no replacement for it either"
+    )]
     pub fn available_permits(self: &Arc<Self>) -> usize {
-        self.permits.load(Ordering::Relaxed)
+        *self.permits.lock().unwrap()
     }
 
     /// Tries to get a `Permit`. If no more permits can be given, it will wait for one permit to be given back from a thread so that it can return a new `Permit`.
-    #[allow(unused_must_use)]
     pub fn acquire(self: &Arc<Self>) -> Permit {
-        loop {
-            if self.permits.load(Ordering::Acquire) != 0 {
-                self.permits.fetch_sub(1, Ordering::AcqRel);
-                return Permit {
-                    semaphore: Arc::clone(self),
-                };
-            }
-            let guard = self.mutex.lock().unwrap();
-            self.condvar.wait(guard).unwrap();
+        let mut permits = self.permits.lock().unwrap();
+        while *permits == 0 {
+            permits = self.condvar.wait(permits).unwrap();
+        }
+        *permits -= 1;
+        Permit {
+            semaphore: Arc::clone(self),
         }
     }
 
     /// Tries to get a `Option<Permit>`. If no more permits can be given, it will return `None`.
     pub fn try_acquire(self: &Arc<Self>) -> Option<Permit> {
-        if self.permits.load(Ordering::Acquire) != 0 {
-            self.permits.fetch_sub(1, Ordering::Release);
-            return Some(Permit {
+        let mut permits = self.permits.lock().unwrap();
+        if *permits > 0 {
+            *permits -= 1;
+            Some(Permit {
                 semaphore: Arc::clone(self),
-            });
+            })
+        } else {
+            None
         }
-        None
     }
 
     /// Releases a permit. This is what `drop()` on `Permit` calls, ideally use `drop(permit)`.
     pub fn release(&self) {
-        self.permits.fetch_add(1, Ordering::Release);
+        let mut permits = self.permits.lock().unwrap();
+        *permits += 1;
         self.condvar.notify_one();
     }
 }
